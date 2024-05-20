be3e6b2b728c22676010b993b9f8c6a1ce0a46e4
Mike Knapp <micknapp@gmail.com>
2024-05-20 11:20:52 +1000

    Style dark theme toggle

--- ui/src/components/nav/Header.tsx
+++ ui/src/components/nav/Header.tsx
@@ -37,7 +37,14 @@ export function Header() {
         </Tooltip>
 
         <Tooltip content="Toggle Dark Mode">
-          <DarkThemeToggle />
+          <DarkThemeToggle
+            theme={{
+              root: {
+                base: "rounded-lg p-2.5 text-sm text-white hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700",
+                icon: "h-6 w-6 text-white",
+              },
+            }}
+          />
         </Tooltip>
       </div>
     </div>
