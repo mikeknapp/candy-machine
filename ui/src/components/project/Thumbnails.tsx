634a02806a31ba503036129511519b5198d5c911
Mike Knapp <micknapp@gmail.com>
2024-05-22 10:44:47 +1000

    Lazy load images

--- ui/src/components/project/Thumbnails.tsx
+++ ui/src/components/project/Thumbnails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { API_BASE_URL } from "../../api";
 import { Project, navigateImages } from "../../models/project";
@@ -7,6 +7,9 @@ import {
   showCropImageModalAtom,
 } from "../../state/atoms";
 import { ProgressPieChart } from "../nav/ProgressPieChart";
+
+const CONTAINER_WIDTH = 220;
+const THUMBNAIL_WIDTH = 170;
 
 export const scrollToThumbnail = (img: string) => {
   const EXTRA_PADDING = 50;
@@ -20,6 +23,9 @@ export const scrollToThumbnail = (img: string) => {
 };
 
 export function Thumbnails({ project }: { project: Project }) {
+  const observer = useRef<IntersectionObserver | null>(null);
+  const imgRefs = useRef<(HTMLImageElement | null)[]>([]);
+
   const [currentProject, setCurrentProject] = useRecoilState(
     currentProjectSelector,
   );
@@ -30,6 +36,7 @@ export function Thumbnails({ project }: { project: Project }) {
     scrollToThumbnail(img);
   };
 
+  // Keyboard navigation.
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (editImageModalIsOpen) return;
@@ -56,14 +63,58 @@ export function Thumbnails({ project }: { project: Project }) {
     };
   }, [project]);
 
+  // Lazy load the thumbnails.
+  useEffect(() => {
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+
+    observer.current = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target as HTMLImageElement;
+          if (img.dataset.src) {
+            img.src = img.dataset.src;
+            img.removeAttribute("data-src");
+          }
+        }
+      });
+    });
+
+    imgRefs.current.forEach((img) => img && observer.current?.observe(img));
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, [project]);
+
+  const getHeight = (img: string) => {
+    // Extract the height and width from the image ID i.e. f4227273f1f071f_589x753.png
+    const matches = img.match(/_(\d+)x(\d+)\./);
+    if (!matches) {
+      return undefined;
+    }
+    const width = parseInt(matches[1]);
+    const height = parseInt(matches[2]);
+    const scaledHeight = Math.round((THUMBNAIL_WIDTH * height) / width);
+    return { height: `${scaledHeight}px` };
+  };
+
   return (
-    <div className="flex h-full w-[220px] flex-col overflow-y-auto bg-slate-800 pb-20 scrollbar-thin dark:bg-slate-900">
-      {project.images.map((img) => (
+    <div
+      className={`flex h-full w-[${CONTAINER_WIDTH}px] flex-col overflow-y-auto bg-slate-800 pb-20 scrollbar-thin dark:bg-slate-900`}
+    >
+      {project.images.map((img, index) => (
         <img
+          ref={(el) => (imgRefs.current[index] = el)}
           key={img}
           id={`thumb-img-${img}`}
+          style={getHeight(img)}
           className={`m-4 cursor-pointer rounded-md border-4 shadow-md ${project.selectedImage != img ? "opacity-30 hover:border-gray-800" : "border-primary-600"} `}
-          src={`${API_BASE_URL}/project/${project.name}/imgs/${img}`}
+          data-src={`${API_BASE_URL}/project/${project.name}/imgs/${img}`}
+          src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
           alt={img}
           onClick={() => selectNewImage(img)}
         />
