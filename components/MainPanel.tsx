2d13dc93267529a73fdc6b94904712579eb8b5cf
Mike Knapp <micknapp@gmail.com>
2025-03-30 12:34:50 +1100

    Break out image into its own component

--- components/MainPanel.tsx
+++ components/MainPanel.tsx
@@ -4,20 +4,14 @@ import { TooltipButton } from "@/components/ui/tooltip-button"
 import { projectImagesAtom, selectedImageIdAtom, selectedProjectAtom } from "@/lib/atoms"
 import { useAtom, useAtomValue } from "jotai"
 import { ArrowLeft, ArrowRight, Copy, RotateCcw, Trash2 } from "lucide-react"
-import Image from "next/image"
-import { useEffect, useState } from "react"
+import { ImageDisplay } from "./images/ImageDisplay"
 import { ImageInfoPanel } from "./images/ImageInfoPanel"
 import { ProcessingQueue } from "./upload/ProcessingQueue"
 
 export const MainPanel = () => {
-  const [isLoading, setIsLoading] = useState(true)
   const selectedProject = useAtomValue(selectedProjectAtom)
   const images = useAtomValue(projectImagesAtom)
   const [selectedImageId, setSelectedImageId] = useAtom(selectedImageIdAtom)
-
-  useEffect(() => {
-    setIsLoading(true)
-  }, [selectedImageId])
 
   const handlePrevImage = () => {
     if (!images.length || selectedImageId === null) return
@@ -95,16 +89,7 @@ export const MainPanel = () => {
       <div className="flex h-full pt-12">
         {/* Image Display */}
         <div className="flex-1 relative flex items-center justify-center bg-gray-100 dark:bg-gray-800 [background-image:linear-gradient(#e5e7eb_1px,transparent_1px),linear-gradient(to_right,#e5e7eb_1px,transparent_1px)] dark:[background-image:linear-gradient(#374151_1px,transparent_1px),linear-gradient(to_right,#374151_1px,transparent_1px)] [background-size:20px_20px]">
-          <Image
-            src={`/data/${selectedProject.slug}/${selectedImage.id}-original.${selectedImage.extension}`}
-            alt={`Image ${selectedImage.id}`}
-            className="max-h-[70%] max-w-full w-auto h-auto object-contain"
-            width={2000}
-            height={2000}
-            priority
-            draggable={false}
-            onLoad={() => setIsLoading(false)}
-          />
+          <ImageDisplay projectSlug={selectedProject.slug} image={selectedImage} />
           <ImageInfoPanel image={selectedImage} />
         </div>
 
